e8c7fad539022ad2dec9ea8c6aa889867cf19d65
MacLane Wilkison <mwilkison@gmail.com>
2014-12-19 14:25:48 -0500

    make contract detail view full screen, fix contract tabs

--- js/index.js
+++
@@ -1,5 +0,0 @@
-var tabs = document.querySelector('paper-tabs');
-tabs.addEventListener('core-select', function() {
-  var p = document.querySelector('core-animated-pages');
-  p.selected = tabs.selected;
-})
